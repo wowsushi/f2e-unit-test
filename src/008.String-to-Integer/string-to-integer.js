af755a5c5bd4892af63e5740977cd3a2c0f96a72
betty713Chen <s9654025@gmail.com>
2020-06-12 17:54:20 +0800

    2nd case pass

--- src/008.String-to-Integer/string-to-integer.js
+++ src/008.String-to-Integer/string-to-integer.js
@@ -4,14 +4,20 @@
  */
 const myAtoi = function(str) {
     let value = 0;
+    let plusMinusSign = ""
 
     for (let i = 0; i <= str.length; i++) {
+        if (str[i] === "+" || str[i] === "-") {
+            plusMinusSign = str[i]
+        }
+
         if ( !isNaN(parseInt(str[i]), 10) ) {
             value += parseInt(str[i], 10) * 10 ** ( str.length - i - 1)
         }
+
     }
     
-    return value
+    return plusMinusSign === "-" ? value * -1 : value
 };
 
 export default myAtoi;
