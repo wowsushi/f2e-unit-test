582eaa561edd219799a145965da49d47dbf8f06d
betty713Chen <betty713.chen@ehsn.com.tw>
2020-06-12 17:25:08 +0800

    1st case pass

--- src/008.String-to-Integer/string-to-integer.js
+++ src/008.String-to-Integer/string-to-integer.js
@@ -3,6 +3,15 @@
  * @return {number}
  */
 const myAtoi = function(str) {
+    let value = 0;
 
+    for (let i = 0; i <= str.length; i++) {
+        if ( !isNaN(parseInt(str[i]), 10) ) {
+            value += parseInt(str[i], 10) * 10 ** ( str.length - i - 1)
+        }
+    }
+    
+    return value
 };
+
 export default myAtoi;
