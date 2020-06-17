aa071880d3a28b1b3ac15cf4f21ea7861da8f9a9
betty713Chen <s9654025@gmail.com>
2020-06-17 10:40:43 +0800

    pass leetcode

--- src/008.String-to-Integer/string-to-integer.js
+++ src/008.String-to-Integer/string-to-integer.js
@@ -3,46 +3,37 @@
  * @return {number}
  */
 const myAtoi = function(str) {
-    let value = 0;
-    let plusMinusSign = ""
     let power = 0
+    let value = 0;
+    let newStr = str.trim()
+    let plusMinusSign = "" 
+
+    // 確認第一個字元是不是正負號
+    if (newStr[0] === "-" || newStr[0] === "+") {
+        plusMinusSign = newStr[0]
+        newStr = newStr.slice(1)
+    }
     
-    for (let i = str.length - 1; i >= 0; i--) {
-        console.log(i, value, power)
-        // 如果空格，跳過解析
-        if (str[i] === " ") continue 
+    for (let i = newStr.length - 1; i >= 0; i--) {
 
-        //如果不是正負數也不是數字
-        if (isNaN(parseInt(str[i], 10)) && str[i] !== "-" && str[i] !== "+") {
-
-            //如果還沒開始計算 value，跳過，解析下一個
-            if (value === 0) {
-                continue
-            
-            // 如果已經開始計算，重新開始計算
-            } else if (value > 0) {
-                value = 0
-                power = 0
-            }
-        }
-      
-        // 如果是負號，先存起來
-        if (str[i] === "-") {
-            plusMinusSign = str[i]
+        //如果遇到非數字或空格，重新計算
+        if (isNaN(parseInt(newStr[i], 10)) || newStr[i] === " ") {
+            value = 0
+            power = 0
         }
 
         // 如果是數字，計算數值
-        if ( !isNaN(parseInt(str[i]), 10) ) {
-            value += parseInt(str[i], 10) * 10 ** power
-            power++
+        if ( !isNaN(parseInt(newStr[i]), 10) ) {
+            value += parseInt(newStr[i], 10) * 10 ** power
+            power++                
         }
-
     }
+    
+    value = plusMinusSign === "-" ? value * -1 : value
 
-    if (value > 2 ** 31 - 1) value = 2 ** 31
-    
-    
-    return plusMinusSign === "-" ? value * -1 : value
+    if (value > 2 ** 31 - 1) return 2 ** 31 - 1
+    if (value <  2 ** 31 * -1) return  2 ** 31 * -1
+    return value  
 };
 
 export default myAtoi;
